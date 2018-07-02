bb2066d6d4757e80e5da53d7a585253d3466789d
Eli Zucker <eliszucker@gmail.com>
2018-07-02 14:22:04 -0700

    changed face enlargement in script

--- faceeyedetect.py
+++ faceeyedetect.py
@@ -1,18 +1,43 @@
 import numpy as np
 import cv2 as cv
-face_cascade = cv.CascadeClassifier('haarcascadeface.xml')
-eye_cascade = cv.CascadeClassifier('haarcascadeeye.xml')
-img = cv.imread('eli.jpg')
+
+#calibrates face box enlargement after detection
+TOP_SCALER = 0.56
+SIDES_SCALER = 0.28
+
+output = []
+
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_alt.xml')
+eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+img = cv.imread('group-people-wearing-sunglasses-29654821.jpg')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+for i in range(len(faces)):
+
+    x = faces[i, 0]
+    y = faces[i, 1]
+    w = faces[i, 2]
+    h = faces[i, 3]
+
+    #draw rectangle around face
+    cv.rectangle(img,(x-int(w*SIDES_SCALER), y-int(h*TOP_SCALER)),(x+w+int(w*SIDES_SCALER),y+h+int(h*SIDES_SCALER)),(255,0,0),2)
+
+    #add face x, y, width, and height to output
+    output.append([x-int(w*SIDES_SCALER), y-int(h*TOP_SCALER), w+2*int(w*SIDES_SCALER), h+int(h*SIDES_SCALER)+int(h*TOP_SCALER)])
+
+print 'convert group-people-wearing-sunglasses-29654821.jpg -crop ' 
+print output[2][2]
+print 'x'
+print output[2][3]
+print '+'
+print output[2][0]
+print '+'
+print output[2][1]
+
 cv.imshow('img',img)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+
