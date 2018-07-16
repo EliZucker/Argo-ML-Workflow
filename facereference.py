5cd38c2503e91e6aa599a362262ff369322228dc
Edan Sneh <edan.sneh@gmail.com>
2018-07-16 14:55:26 -0700

    Face working

--- facereference.py
+++ facereference.py
@@ -1,7 +1,8 @@
 import face_recognition
 import json
+import sys
 
-arr = ["/src/muka.jpg", "/src/alex.jpg", "/src/jesse.jpg"]
+arr = ["src/muka.jpg", "src/alex.jpg", "src/jesse.jpg"]
 unknown_image = face_recognition.load_image_file("tmp/photo.jpg")
 unknown_encoding = face_recognition.face_encodings(unknown_image)[0]
 
@@ -10,5 +11,5 @@ for image in arr:
     known_encoding = face_recognition.face_encodings(known_image)[0]
     if face_recognition.compare_faces([known_encoding], unknown_encoding):
         print(True)
-
+        sys.exit()
 print(False)
