66bbd9b7bade35855ae9f52dde3c3baee431b9d2
Edan Sneh <edan.sneh@gmail.com>
2018-07-16 14:38:43 -0700

    facereference edits

--- facereference.py
+++ facereference.py
@@ -9,5 +9,6 @@ for image in arr:
     known_image = face_recognition.load_image_file(image)
     known_encoding = face_recognition.face_encodings(known_image)[0]
     if face_recognition.compare_faces([known_encoding], unknown_encoding):
-        print "True"
-print "False"
+        print(True)
+
+print(False)
