66b8d40924383592f859afb6632822db60acfba6
Edan Sneh <edan.sneh@gmail.com>
2018-07-23 14:14:46 -0700

    eli will be raged

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 apt-get update -y
-apt-get install -y git dlib
+apt-get install -y git vim sudo wget man gawk libgoogle-glog-dev libboost-all-dev python-numpy python-matplotlib cmake build-essential
+echo "INSTALLING PIP"
+mkdir -p ~/pip/
+wget -O ~/pip/install.py https://bootstrap.pypa.io/get-pip.py
+python ~/pip/install.py
+rm -r ~/pip
+pip install visvis imageio dlib
 git clone https://github.com/YadiraF/PRNet
-pip install -U scikit-image
+# pip install -U scikit-image
 cd PRNet/Data/net-data
 curl -o 256_256_resfcn256_weight.data-00000-of-00001 https://github.com/EliZucker/Argo-Workflow-Test/blob/master/256_256_resfcn256_weight.data-00000-of-00001?raw=true -L
 cd ..
