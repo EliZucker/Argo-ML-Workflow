242862796590cb2329aff4e8705b61b2ea9653b9
Edan Sneh <edan.sneh@gmail.com>
2018-07-23 13:43:40 -0700

    eli will be raged

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 git clone https://github.com/YadiraF/PRNet
-cd PRNet
-# curl https://raw.githubusercontent.com/EliZucker/Argo-Workflow-Test/master/mnist.py | python
+cd PRNet/Data/net-data
+curl -o 256_256_resfcn256_weight.data-00000-of-00001 https://github.com/EliZucker/Argo-Workflow-Test/blob/master/256_256_resfcn256_weight.data-00000-of-00001?raw=true -L
+cd ..
+cd ..
+mkdir inputdir
+mkdir outputdir
+cd inputdir
+curl -o muka.jpg https://github.com/EliZucker/Argo-Workflow-Test/blob/master/muka.jpg
+cd ..
 python run_basics.py
+python demo.py -i inputdir -o outputdir --isDlib True
