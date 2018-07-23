28d6d0811e9074d6290183ce62b4e5607ba08855
Edan Sneh <edan.sneh@gmail.com>
2018-07-23 14:00:26 -0700

    eli will be raged

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 apt-get update -y
 apt-get install -y git
 git clone https://github.com/YadiraF/PRNet
+pip install -U scikit-image
 cd PRNet/Data/net-data
 curl -o 256_256_resfcn256_weight.data-00000-of-00001 https://github.com/EliZucker/Argo-Workflow-Test/blob/master/256_256_resfcn256_weight.data-00000-of-00001?raw=true -L
 cd ..
