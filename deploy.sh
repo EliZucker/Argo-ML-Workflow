21c97100fa69da44bf1b865b140b8acf6877b1dc
Edan Sneh <edan.sneh@gmail.com>
2018-07-23 13:55:07 -0700

    eli will be raged

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 apt-get update -y
 apt-get install -y git
 git clone https://github.com/YadiraF/PRNet
-# cd PRNet/Data/net-data
-# curl -o 256_256_resfcn256_weight.data-00000-of-00001 https://github.com/EliZucker/Argo-Workflow-Test/blob/master/256_256_resfcn256_weight.data-00000-of-00001?raw=true -L
-# cd ..
-# cd ..
-# mkdir inputdir
-# mkdir outputdir
-# cd inputdir
-# curl -o muka.jpg https://github.com/EliZucker/Argo-Workflow-Test/blob/master/muka.jpg
-# cd ..
-# python run_basics.py
-# python demo.py -i inputdir -o outputdir --isDlib True
+cd PRNet/Data/net-data
+curl -o 256_256_resfcn256_weight.data-00000-of-00001 https://github.com/EliZucker/Argo-Workflow-Test/blob/master/256_256_resfcn256_weight.data-00000-of-00001?raw=true -L
+cd ..
+cd ..
+mkdir inputdir
+mkdir outputdir
+cd inputdir
+curl -o muka.jpg https://github.com/EliZucker/Argo-Workflow-Test/blob/master/muka.jpg
+cd ..
+python run_basics.py
+python demo.py -i inputdir -o outputdir --isDlib True
